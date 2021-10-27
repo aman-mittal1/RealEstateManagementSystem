ffea8c62e3f2f3e7d86bddc440e61f90ff4cb530
Ankita-Harkude <60356895+Ankita-Harkude@users.noreply.github.com>
2021-10-27 20:49:59 +0530

    changes done using mappedsuperclass

---
+++ RealEstateProject/src/main/java/com/mph5/model/Seller.java
@@ -0,0 +1,58 @@
+package com.mph5.model;
+
+import java.util.List;
+
+
+import javax.persistence.Column;
+import javax.persistence.CascadeType;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Seller {
+	@Id
+	private int sellerId;
+	
+	@Column
+	@Embedded
+	private Customer customer;
+	
+	@OneToMany(mappedBy = "seller", cascade = CascadeType.ALL)
+	private List<Property> property;
+
+
+	  public Seller(int sellerId, Customer customer) {
+		super();
+		this.sellerId = sellerId;
+		this.customer = customer;
+	}
+
+	  public Seller() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	  public int getSellerId() {
+		return sellerId;
+	}
+
+	  public void setsellerId(int sellerId) {
+		this.sellerId = sellerId;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	@Override
+	public String toString() {
+		return "Seller [sellerId=" + sellerId + ", customer=" + customer + "]";
+	}
+	
+	}
